74c7bc91be10085e111ed3d52a7699f82b2374b5
GustavMaki <gustav.siljamaki@gmail.com>
2020-09-07 10:06:27 +0200

    Uppgift 2 steg 1 klar

---
+++ U200907X1/U20200907.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace U20200907._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ditt citat här: ");
+            string Citat = Console.ReadLine();
+        }
+    }
+}
