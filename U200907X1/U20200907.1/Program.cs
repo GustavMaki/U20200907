3de0b8f06f7e901224a83169195867c3aba048ab
GustavMaki <gustav.siljamaki@gmail.com>
2020-09-07 09:56:40 +0200

    skriv ut antal ord klart

--- U200907X1/U20200907.1/Program.cs
+++ U200907X1/U20200907.1/Program.cs
@@ -6,8 +6,24 @@ namespace U20200907._1
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            int i = 0;
+            int WordCount = 1;
+
             Console.WriteLine("Skriv ditt citat här: ");
             string Citat = Console.ReadLine();
+
+            while (n <= Citat.Length - 1)
+            {
+                if (Citat[n] == ' ')
+                {
+                    WordCount++;
+                }
+                n++;
+            }
+
+            Console.WriteLine("Antalet ord i ditt citat är: " + WordCount);
+            string[] Words = Citat.Split(" ");
         }
     }
 }
