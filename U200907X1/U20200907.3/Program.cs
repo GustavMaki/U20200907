425f0783a46f37527158732f09a7b93b989e0bec
GustavMaki <gustav.siljamaki@gmail.com>
2020-09-07 13:40:10 +0200

    Work in progress...har lyckats ränka vilka ord som återkommer och kan skriva ut dem. Dock så skrivs just nu även dublicationerna ut. Försökte lösa det med array.Duplicates men då slutade den räkna vissa ord. Kan i nuläget inte lista ut hur man löser det.

---
+++ U200907X1/U20200907.3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace U20200907._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Skriv ditt citat här: ");
+                string Citat = Console.ReadLine();
+
+                string[] Words = Citat.Split(" ");
+                int[] count = new int[Words.Length];
+
+                foreach (string word in Words)
+                {
+
+                    for (int i = 0; i < Words.Length; i++)
+                    {
+
+                        for (int j = 0; j < Words.Length; j++)
+                        {
+
+                            if (Words[i] == Words[j])
+                                count[i]++;
+                        }
+
+                        Console.WriteLine( Words[i] + " (" + count[i] + ")");
+                    }
+
+
+
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
+
